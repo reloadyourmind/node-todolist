061e8207d1cd42c340c437154305670e90020f4a
reloadyourmind <amdechannel@gmail.com>
2020-07-14 11:34:17 +0300

    Added CRUD. Need to FIX UPDATE!

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.listen(3000, () => console.log("Server Up and running"));
 
 
 
-//GET METHOD
+// GET METHOD
 app.get("/", (req, res) => {
     TodoTask.find({}, (err, tasks) => {
         res.render("todo.ejs", { todoTasks: tasks });
@@ -31,8 +31,9 @@ app.get("/", (req, res) => {
     
 });
 
-//POST METHOD
-app.post('/',async (req, res) => {
+// POST METHOD
+app.
+post('/',async (req, res) => {
     const todoTask = new TodoTask({
     content: req.body.content
     });
@@ -43,4 +44,34 @@ app.post('/',async (req, res) => {
         res.redirect("/");
     }
 });
+
+// UPDATE
+
+app
+.route("/edit/:id")
+.get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todoEdit.ejs", {todoTasks: tasks, idTask: id});
+    });
+})
+.post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, {content: req.body.content}, err => {
+        if (err) return res.send(500, err);
+    });
+});
+
+// DELETE
+
+app
+.route("/remove/:id")
+.get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
+
 
